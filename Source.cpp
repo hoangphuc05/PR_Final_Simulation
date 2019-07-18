fe04d5354d4b8fbab6c63b504d965ed114a975f1
Phuc Hoang <chphuc05@gmail.com>
2019-07-18 14:57:35 +0700

    add the menu

--- Source.cpp
+++ Source.cpp
@@ -16,16 +16,43 @@
 #include "Random.h"
 #include "Doctor.h"
 #include "Hospital.h"
+#include "ReadInt.h"
 
 using namespace std;
 Random myRandom;
 
 int main() {
+	std::cout << "Welcome to simulation" << std::endl;
 	Hospital hos273;
 
 	hos273.run();
 	
-	
-	
+	char choice;
+	do {
+		cout << endl;
+		int select;
+		string	menu_string = "Please choose an action below:\n";
+		menu_string += "0 - List all the name that were treated\n";
+		menu_string += "1 - Retrieve record by name\n";
+		menu_string += "Enter: ";
+		select = read_int(menu_string, 0, 4);
+
+		// Perform the banking activity based on the user selection
+		switch (select) {
+		case 0:
+			hos273.listAllName();
+			break;
+		case 1:
+			hos273.printRecord();
+			break;
+		default:
+			cout << "Goodbye! Thank you for visiting.\n";
+			return 0;
+		}
+
+		cout << "Do you wish to perform another transaction? (y or n): ";
+		cin >> choice;
+	} while (choice != 'n');
+
 	system("pause");
 }
